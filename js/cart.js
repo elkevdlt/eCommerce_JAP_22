5054d2df3618c2def44cbc14a961cd490a43de43
elkevdlt <67130091+elkevdlt@users.noreply.github.com>
2022-10-30 20:52:47 -0300

    Add validation cart and subtotal

--- js/cart.js
+++ js/cart.js
@@ -33,19 +33,129 @@ document.addEventListener('DOMContentLoaded', function () {
         </div>
       </div>
       `
-      /*  total()
-       function total(valor) {
-         let costo = document.getElementById("costoCart").value
-         costo.innerHTML += `${dato.currency} ${dato.unitCost * valor}`;
- 
-       } */
-      let costo = dato.unitCost;
+
+
+      let costo = parseInt(dato.unitCost);
       let moneda = dato.currency;
       let cantidad = document.getElementById("inputCart")
+
       cantidad.addEventListener('change', () => {
+        subtotal = cantidad.value * costo;
         /* console.log("cambiaste"); */
-        document.getElementById("costoCart").innerHTML = moneda + " " + cantidad.value * costo;
+        //chequeado(subtotal)
+        document.getElementById("costoCart").innerHTML = moneda + " " + subtotal
+        let costoEnvio = document.getElementById("subtotalInfo")
+        costoEnvio.innerHTML = `
+        <table class="table align-middle">
+          <thead>
+            <tr>
+              <th>Subtotal</th>
+              <th>Envio</th>
+              <th>Total</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>${moneda + " " + subtotal}</td> <td>${moneda + " " + chequeado(subtotal)}</td> <td>${moneda} ${subtotal + chequeado(subtotal)}</td>
+            </tr>
+          </tbody>
+
+        </table>
+        
+          `
       })
     }
   })
+
+  function chequeado(subtotal) {
+    let envio1 = document.getElementById("envio1");
+    let envio2 = document.getElementById("envio2");
+    let envio3 = document.getElementById("envio3");
+    /* console.log(parseFloat(envio1.value) * subtotal);
+    console.log(parseFloat(envio2.value) * subtotal); //aca lo muestra en consola
+    console.log(parseFloat(envio3.value) * subtotal); */
+    if (envio1.checked) {
+      /* console.log("soy envio1 del 15 por ciento");
+      console.log(parseFloat(envio1.value) * subtotal); */ //pero aca no, da NaN ¿?
+      let valorEnvio = parseFloat(envio1.value) * subtotal;
+      return valorEnvio;
+
+    }
+    if (envio2.checked) {
+      /*  console.log("soy envio2 del 7 por ciento");
+       console.log(parseFloat(envio2.value) * subtotal); */
+      let valorEnvio = parseFloat(envio2.value) * subtotal;
+      return valorEnvio;
+    }
+    if (envio3.checked) {
+      /* console.log("soy envio3 del 5 por ciento");
+      console.log(parseFloat(envio3.value) * subtotal); */
+      let valorEnvio = parseFloat(envio3.value) * subtotal;
+      return valorEnvio;
+    }
+  }
 })
+
+let dep = document.getElementById("dep");
+let tar = document.getElementById("tar");
+let calle = document.getElementById("calle");
+let numero = document.getElementById("numero");
+let esquina = document.getElementById("esquina");
+document.getElementById("btnFinaliza").addEventListener('click', function (e) {
+  if ((envio1.checked != true) && (envio2.checked != true) && (envio3.checked != true)) {
+    alert('Debe elegir un tipo de envío');
+  }
+  if ((dep.checked != true) && (tar.checked != true)) {
+    alert('Debe seleccionar un método de pago!');
+  }
+})
+
+let credito1 = document.getElementById("creditCard1");
+let credito2 = document.getElementById("creditCard2");
+let credito3 = document.getElementById("creditCard3");
+credito1.disabled = true;
+credito2.disabled = true;
+credito3.disabled = true;
+document.getElementById('dep').addEventListener('click', function (e) {
+  credito1.disabled = true;
+  credito2.disabled = true;
+  credito3.disabled = true;
+  deposito.disabled = false;
+});
+
+let deposito = document.getElementById("deposito");
+deposito.disabled = true;
+document.getElementById('tar').addEventListener('click', function (e) {
+  deposito.disabled = true;
+  credito1.disabled = false;
+  credito2.disabled = false;
+  credito3.disabled = false;
+});
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if ((calle.value > "") && (numero.value > "") && (esquina.value > "")) {
+        alert('Su compra ha sido efectuada de forma exitosa!')
+      }
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
+
+/* 
+function subTotal(moneda, costo) {
+} */
